e6ffc06fa27fbbc206639b4e3869b605d6d8ba4b
Tharindu Amarasinghage <tamarasinghage@csumb.edu>
2026-04-20 13:29:47 -0700

    Created SceneFactory shell

---
+++ src/main/java/dungeonfighter/enums/SceneType.java
@@ -0,0 +1,13 @@
+package dungeonfighter.enums;
+
+/**
+ * Explanation:
+ *
+ * @author Tharindu Amarasinghage
+ * @since 4/20/26
+ */
+public enum SceneType {
+    MAIN,
+    LOGIN,
+    DASHBOARD
+}
--- src/main/java/dungeonfighter/util/SceneFactory.java
+++ src/main/java/dungeonfighter/util/SceneFactory.java
@@ -1,5 +1,9 @@
 package dungeonfighter.util;
 
+import dungeonfighter.enums.SceneType;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 /**
  * Explanation:
  *
@@ -7,4 +11,28 @@ package dungeonfighter.util;
  * @since 4/20/26
  */
 public class SceneFactory {
+
+    public static Scene create (SceneType type, Stage stage){
+        return switch (type) {
+            case MAIN -> buildMainScene(stage);
+            case LOGIN -> buildLoginScene (stage);
+            case DASHBOARD -> buildDashboardScene(stage);
+
+        };
+    }
+
+    private static Scene buildMainScene(Stage stage) {
+        /* TODO */
+        return null;
+    }
+
+    private static Scene buildLoginScene(Stage stage) {
+        /* TODO */
+        return null;
+    }
+
+    private static Scene buildDashboardScene(Stage stage) {
+        /* TODO */
+        return null;
+    }
 }
