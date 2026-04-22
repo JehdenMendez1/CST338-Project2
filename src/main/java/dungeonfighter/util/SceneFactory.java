d738bf558a117f97c397a6b09718a9819f0b796e
Tharindu Amarasinghage <tamarasinghage@csumb.edu>
2026-04-22 10:41:18 -0700

    Added JDBC Dependency to build.gradle

--- src/main/java/dungeonfighter/util/SceneFactory.java
+++ src/main/java/dungeonfighter/util/SceneFactory.java
@@ -1,10 +1,14 @@
 package dungeonfighter.util;
 
 import dungeonfighter.enums.SceneType;
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -15,6 +19,11 @@ import javafx.stage.Stage;
  * @since 4/20/26
  */
 public class SceneFactory {
+    private static final int SCENE_WIDTH = 1200;
+    private static final int SCENE_HEIGHT = 1000;
+    private static final String USERNAME_PROMPT = "USERNAME";
+    private static final String PASSWORD_PROMPT = "PASSWORD";
+
 
     public static Scene create (SceneType type, Stage stage){
         return switch (type) {
@@ -26,30 +35,84 @@ public class SceneFactory {
     }
 
     private static Scene buildMainScene(Stage stage) {
+
+        Label mainSceneLabel = new Label("MAIN MENU");
+        mainSceneLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-alignment: center;");
+
+        Button playMainMenu = new Button("PLAY");
+        Button buildDeckMainMenu = new Button("BUILD DECK");
+        Button myCardsMainMenu = new Button("MY CARDS");
+        Button scoresMainMenu = new Button("SCORES");
+        Button logoutMainMenu = new Button("LOGOUT");
+
+        VBox layoutMainMenu = new VBox();
+        layoutMainMenu.setSpacing(16);
+        layoutMainMenu.getChildren().addAll(mainSceneLabel, playMainMenu,
+                buildDeckMainMenu, myCardsMainMenu, scoresMainMenu, logoutMainMenu);
+
+        layoutMainMenu.setAlignment(Pos.CENTER_LEFT);
+
         /* TODO */
-        return null;
+        return new Scene(layoutMainMenu, SCENE_WIDTH, SCENE_HEIGHT);
     }
 
     private static Scene buildLoginScene(Stage stage) {
 
         Label titleLabel = new Label("Welcome to the Dungeons Fighter");
         titleLabel.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-alignment: center;");
+        titleLabel.setAlignment(Pos.TOP_CENTER);
 
+        // Username Text Filed
+        TextField userName = new TextField();
+        userName.setPromptText(USERNAME_PROMPT);
+        userName.setPrefWidth(100);
+        String userNameInput = userName.getText();
+
+
+        // Password Text Field
+        TextField password = new PasswordField();
+        password.setPromptText(PASSWORD_PROMPT);
+        password.setPrefWidth(100);
+        String passwordInput = password.getText();
+
+        // Login Button
         Button loginButton = new Button("LOGIN");
 
         loginButton.setOnAction(e ->
                 stage.setScene(create(SceneType.MAIN, stage))
         );
 
-        VBox layout = new VBox(16, titleLabel, loginButton);
-        layout.setAlignment(Pos.CENTER);
-
-
-
-
+        // Register Button
         Button registerButton = new Button("REGISTER");
         /* TODO */
-        return new Scene(layout, 800, 600);
+
+
+        // Login Page Arrangement
+        VBox vBoxLeft = new VBox();
+
+        VBox vBoxRight = new VBox();
+        vBoxRight.setPadding(new Insets(50));
+        vBoxRight.setSpacing(20);
+        vBoxRight.getChildren().addAll( userName, password, loginButton, registerButton);
+
+        HBox layout = new HBox();
+        layout.getChildren().addAll(vBoxLeft,vBoxRight);
+
+
+        //layout.getChildren().addAll(titleLabel);
+        layout.setPadding(new Insets(50));
+        layout.setAlignment(Pos.CENTER);
+
+        VBox mainVBOX = new VBox();
+        mainVBOX.setAlignment(Pos.CENTER);
+        mainVBOX.setPadding(new Insets(20));
+        mainVBOX.getChildren().addAll(titleLabel, layout);
+
+
+
+
+
+        return new Scene(mainVBOX, SCENE_WIDTH, SCENE_HEIGHT);
     }
 
     private static Scene buildDashboardScene(Stage stage) {
